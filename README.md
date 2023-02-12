eeb886031e8ca33cfc385cd7c8f477b68ccba81e
isham993 <d.isham.993@gmail.com>
2023-02-13 01:32:22 +0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# The spelled-out intro to neural networks and backpropagation: building micrograd
+
+Scribbled notes from the [first tutorial](https://youtu.be/VMj-3S1tku0?list=PLAqhIrjkxbuWI23v9cThsA9GvCAUhRvKZ) in the series Neural Networks: Zero to Hero by Dr. Andrej Karpathy.
+
+For graph visualizations install graphviz (Mac OS):
+
+```
+pip install graphviz
+```
+
+```
+brew install graphviz
+```
---
+++ value.py
@@ -0,0 +1,64 @@
+import math
+
+class Value:
+    def __init__(self, data, _children=(), _op="", label=""):
+        self.data = data
+        self.grad = 0.0
+        self._backward = lambda: None
+        self._prev = set(_children)
+        self._op = _op
+        self.label = label
+
+    def __repr__(self):
+        return f"Value(data={self.data})"
+
+    def __add__(self, other):
+        out = Value(self.data + other.data, (self, other), "+")
+
+        def _backward():
+            self.grad += 1.0 * out.grad
+            other.grad += 1.0 * out.grad
+
+        out._backward = _backward
+
+        return out
+
+    def __mul__(self, other):
+        out = Value(self.data * other.data, (self, other), "*")
+
+        def _backward():
+            self.grad += other.data * out.grad
+            other.grad += self.data * out.grad
+
+        out._backward = _backward
+
+        return out
+
+    def tanh(self):
+        x = self.data
+        t = (math.exp(2 * x) - 1) / (math.exp(2 * x) + 1)
+        out = Value(t, (self,), "tanh")
+
+        def _backward():
+            self.grad += (1 - t**2) * out.grad
+
+        out._backward = _backward
+
+        return out
+
+    def backward(self):
+        topo = []
+        visited = set()
+
+        def build_topo(v):
+            if v not in visited:
+                visited.add(v)
+                for child in v._prev:
+                    build_topo(child)
+                topo.append(v)
+
+        build_topo(self)
+
+        self.grad = 1.0
+        for node in reversed(topo):
+            node._backward()
